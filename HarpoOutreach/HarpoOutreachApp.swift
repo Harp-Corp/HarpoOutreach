e1955fe81ac2e8e6b0520e53b053db09a3f36e7c
keiner999 <mf@harpocrates-corp.com>
2026-02-14 17:40:19 +0100

    Initial Commit

---
+++ HarpoOutreach/HarpoOutreachApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarpoOutreachApp.swift
+//  HarpoOutreach
+//
+//  Created by Martin Foerster on 14.02.26.
+//
+
+import SwiftUI
+
+@main
+struct HarpoOutreachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
